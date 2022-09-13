451f00d738741def6fee4131878a42015ba7698f
ljm0850 <dlwoals0850@gmail.com>
2022-09-13 21:36:39 +0900

    obtimise

--- 합승 택시 요금.py
+++ 합승 택시 요금.py
@@ -1,13 +1,13 @@
 import heapq
 def solution(n:int, s:int, a:int, b:int, fares:list)->int:
-    road = [[] for _ in range(n+1)] # 그래프 기록, 1번지점에서 2번 지점
+    road = [[] for _ in range(n+1)] # 그래프 기록, 1번지점에서 2번 지점 road[1][2] = value
     for far in fares:
         road[far[0]].append((far[1],far[2]))
         road[far[1]].append((far[0],far[2]))
     INF = 20000001 # n<=200, 거리가 최대 100,000 // INF는 나올수 없는 높은 값
     answer = INF
     # 최저 비용 기록, 2지점에서 3지점 최저 비용 = shortcut[2][3]
-    shortcut = [[INF]*(n+1) for _ in range(n+1)]
+    shortcut = [[INF]*(n+1) for _ in range(n+1)] # short[1][2] = value
     # 시작위치에서 그대로 있을때는 0만큼 값이 드는것 기록(출발지점에서 바로 찢어지는게 유리할 경우 필요)
     for node in range(1,n+1):
         shortcut[node][node] = 0
@@ -25,10 +25,13 @@ def solution(n:int, s:int, a:int, b:int, fares:list)->int:
                     heapq.heappush(heap,(next_pay,next))
                     shortcut[start][next] = next_pay
     # 모든 지점에서 최저 비용 기록
-    for start in range(1,n+1):
+    # for start in range(1,n+1):
+    #     dijkstra(start)
+    for start in (s,a,b):
         dijkstra(start)
 
     # 중간지점까지 같이 가고 각자 나눠 간다는 개념(시작점->중간지점 + 중간지점-> a지점 + 중간지점->b지점)
     for middle_node in range(1,n+1):
-        answer = min(answer,shortcut[s][middle_node]+shortcut[middle_node][a]+shortcut[middle_node][b])
+        # answer = min(answer,shortcut[s][middle_node]+shortcut[middle_node][a]+shortcut[middle_node][b])
+        answer = min(answer,shortcut[s][middle_node]+shortcut[a][middle_node]+shortcut[b][middle_node])
     return answer
